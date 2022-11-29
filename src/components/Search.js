248be69fe10f0cd3fc8e3b1085b2e2937d38eae9
Kim <kimadamiec36@ymail.com>
2022-11-29 13:33:34 -0700

    initial commit

---
+++ src/components/Search.js
@@ -0,0 +1,71 @@
+import SearchIcon from '@material-ui/icons/Search';
+import MicIcon from '@material-ui/icons/Mic';
+import { Button } from '@material-ui/core';
+import styled from 'styled-components';
+//import { Search } from '@material-ui/icons';
+
+import { useState } from 'react';
+
+const SearchInput = styled.div`
+    display: flex;
+    align-items: center;
+    border: 1px solid lightgray;
+    height: 30px;
+    padding: 10px 15px;
+    width: 500px;
+    margin: 0px auto;
+    margin-top: 40px;
+    border-radius: 999px;
+    input {
+        flex: 1;
+        paddinh: 8px 13px;
+        font-size: medium;
+        border: 0;
+        outline: 0;
+    }
+    .searchicon {
+        color:gray;
+    }
+`
+
+
+const SearchButton = styled.div`
+    margin-top: 20px;
+    display: flex;
+    justify-content: center;
+    button {
+        margin: 5px;
+        background: #f8f8f8 !important;
+        border: 1px solid white;
+        text-transform: inherit;
+        &:hover {
+            margin: 5px;
+            background: #f8f8f8 !important;
+            color: #000;
+            border: 1px solid #c6c6c6
+        }
+    }
+`
+
+
+const Search = ({ hide }) => {
+    const [input, setInput] = useState("");
+
+    return (
+        <form>
+            <SearchInput>
+                <SearchIcon className='searchicon' />
+                <input value={input} onChange={e => setInput(e.target.value)} />
+                <MicIcon />
+                </SearchInput>
+                {!hide && (
+                    <SearchButton>
+                        <Button type="submit" variant="outlined">Google Search</Button>
+                        <Button variant="outlined">I am feeling lucky</Button>
+                    </SearchButton>
+                )}
+        </form>
+    )
+}
+
+export default Search;
